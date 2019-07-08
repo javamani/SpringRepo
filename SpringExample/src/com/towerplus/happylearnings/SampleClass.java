6fa34a4687b6f936d6a41d3a0baec0712f0a9833
javamani <pioneermani@yahoo.com>
2019-07-08 14:44:04 -0400

    Update SampleClass.java
    commiting with remote changes

--- SpringExample/src/com/towerplus/happylearnings/SampleClass.java
+++ SpringExample/src/com/towerplus/happylearnings/SampleClass.java
@@ -4,6 +4,7 @@ public class SampleClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Adding comment to check latest changes reflected in local or not.
 System.out.println("New changes were added");
 	}
 
